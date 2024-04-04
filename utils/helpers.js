f49a31b8a60b185a53a86d6b72013ac935022283
October <slest12@outlook.com>
2024-04-03 23:29:08 -0700

    first commit

---
+++ utils/helpers.js
@@ -0,0 +1,11 @@
+//Uses date) object to parse input date
+function formatDate(date) {
+    return `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${new Date(
+        date
+      ).getFullYear()}`;
+}
+
+
+module.exports = {
+    formatDate
+}
